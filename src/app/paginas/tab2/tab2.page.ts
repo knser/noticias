57e67f8547becaeaed1a4ec5a9770003a2d506ce
Amilcar Aguilar <servisys2016@gmail.com>
2021-02-24 17:53:40 -0400

    clase 20210222

--- src/app/paginas/tab2/tab2.page.ts
+++ src/app/paginas/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Article } from 'src/app/interfaces/interfaces';
+import { NoticiasService } from 'src/app/services/noticias.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,22 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  categorias: any[] = ['business','entertainment','general','health','science','sports','technology'];
+  
+  noticias:Article[];
+
+  constructor(private NoticiasService:NoticiasService) {}
+
+  ngOnInit(){
+
+  }
+
+  segmentChanged($event){
+    console.log($event.detail.value);
+    this.NoticiasService.getCategorias($event.detail.value).subscribe(resp=>{
+      console.log(resp);
+      //this.noticias.push(...resp.Article);
+    })    
+  }
 
 }
--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Titulares } from '../interfaces/interfaces';
+import { environment } from 'src/environments/environment';
+
+
+const apikey=environment.apiKey;
+const apiUrl=environment.apiUrl;
+const headers=new HttpHeaders({
+  'X-Api-key':apikey
+})
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +19,21 @@ export class NoticiasService {
 
   constructor(private httpClient:HttpClient) { }
 
+  private ejecutarServicio<t>(query:string){
+    query = apiUrl + query;
+    console.log(query);
+    return this.httpClient.get<t>(query,{headers});
+  }
+
   getTitulares(){
-    return this.httpClient.get<Titulares>('https://newsapi.org/v2/top-headlines?country=us&apiKey=fdb5419249794b839700a1345cbb393d');
+    return this.ejecutarServicio('/top-headlines?country=us');
+    //return this.httpClient.get<Titulares>('https://newsapi.org/v2/top-headlines?country=us&apiKey=fdb5419249794b839700a1345cbb393d');
+  }
+
+  getCategorias(categoria:string){
+    //return this.ejecutarServicio(`/top-headlines?country=us&category=${categoria}`);
+    //return this.httpClient.get<Titulares>('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fdb5419249794b839700a1345cbb393d');
+    return this.httpClient.get<Titulares>('https://newsapi.org/v2/top-headlines?country=us&apiKey=fdb5419249794b839700a1345cbb393d&category=business');
   }
 
 }
